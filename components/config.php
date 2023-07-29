fa1391d5f237154c762bf885c33042dcb7e04cca
Md Shahinur Rahman <67356028+itsmdshahin@users.noreply.github.com>
2023-07-29 22:05:41 +0600

    Update config.php

--- components/config.php
+++ components/config.php
@@ -8,6 +8,6 @@ $conn = mysqli_connect($serverName, $userName, $password, $dbName);
 if (!$conn) {
     die("Connection error: " . mysqli_connect_error());
 } else {
-    echo "<script>alert('DB CONNECTED!')</script>";
+    echo "";
 }
 ?>
